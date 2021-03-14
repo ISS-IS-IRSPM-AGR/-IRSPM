6b43c9c18164ce8c8a1c52815b5c02d3d85285c6
antoniadevina <21048103+antoniadevina@users.noreply.github.com>
2021-03-15 00:03:40 +0800

    commit

---
+++ MyWebsite/AGR/AGRFrontend/src/tileData.js
@@ -0,0 +1,58 @@
+const tileData = [
+    {
+      img: '/AGRFrontend/static/images/4.jpg',
+      title: 'Breakfast',
+      author: 'jill111',
+      cols: 1,
+      featured: true,
+    },
+    {
+      img: '/AGRFrontend/static/images/5.jpg',
+      title: 'Tasty burger',
+      cols: 2,
+      author: 'director90',
+    },
+    {
+      img: '/AGRFrontend/static/images/8.jpg',
+      title: 'Camera',
+      cols: 2,
+      author: 'Danson67',
+    },
+    {
+      img: '/AGRFrontend/static/images/9.jpg',
+      title: 'Morning',
+      cols: 1,
+      author: 'fancycrave1',
+      featured: true,
+    },
+    {
+      img: '/AGRFrontend/static/images/24.jpg',
+      title: 'Morning',
+      cols: 1,
+      author: 'fancycrave1',
+      featured: true,
+    },
+    {
+      img: '/AGRFrontend/static/images/25.jpg',
+      title: 'Morning',
+      cols: 2,
+      author: 'fancycrave1',
+      featured: true,
+    },
+    {
+      img: '/AGRFrontend/static/images/28.jpg',
+      title: 'Morning',
+      cols: 1,
+      author: 'fancycrave1',
+      featured: true,
+    },
+    {
+      img: '/AGRFrontend/static/images/29.jpg',
+      title: 'Morning',
+      cols: 1,
+      author: 'fancycrave1',
+      featured: true,
+    },
+  ];
+  
+  export default tileData;
